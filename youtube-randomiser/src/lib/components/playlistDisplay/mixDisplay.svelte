88a4a36144a940c00d97d43f7191229b491231c4
ImHamba <callmehamba@gmail.com>
2023-07-19 01:46:07 +1000

    Added fade/fly transition when swapping video in mix player to indicate what direction it is moving in the playlist.

--- youtube-randomiser/src/lib/components/playlistDisplay/mixDisplay.svelte
+++ youtube-randomiser/src/lib/components/playlistDisplay/mixDisplay.svelte
@@ -1,38 +1,77 @@
 <script lang="ts">
+	import { sleep } from '$lib/misc/util';
 	import PlaylistWrapper from './playlistWrapper.svelte';
 	export let videoList: IVideoData[];
 	export let activeVideoIndex = -1;
-	export let swapToIndex;
+	export let swapToIndex: number;
 
 	export const scrollToListIndex = (scrollToIndex: number) => {
-		setTimeout(() => {
-			const liElement = document
-				.getElementById('li-wrapper')
-				?.children.item(scrollToIndex) as HTMLElement;
-			liElement.scrollIntoView(true);
-			liElement.scrollIntoView(true);
-		}, 500);
+		const liElement = document
+			.getElementById('li-wrapper')
+			?.children.item(scrollToIndex) as HTMLElement;
+		liElement.scrollIntoView(true);
+		liElement.scrollIntoView(true);
 	};
 
 	export let searchTerm = '';
 	const containsSubstring = (fullString: string, substr: string) => {
 		return fullString.toLowerCase().includes(substr.toLowerCase());
 	};
+
+	let listElement: HTMLElement[] = [];
+	let transitionAnimTime = 300; // time in ms
+	let directionDown = false;
+	const handleSwapVideo = (index: number) => async () => {
+		// no swapping if clicking on current video
+		if (index == activeVideoIndex) {
+			return;
+		}
+
+		// no fade/fly animation required if the video is the next one, since it is already in the correct place
+		if (index != activeVideoIndex + 1) {
+			if (searchTerm) {
+				directionDown = true
+			}
+
+			let target = listElement[index];
+			target.classList.remove('fade');
+			target.classList.add('fade');
+			await sleep(transitionAnimTime * (2 / 3));
+			target.classList.remove('fade');
+		}
+
+		directionDown = false
+
+		if (searchTerm) {
+			searchTerm = '';
+
+			// sleep is required to scroll to newly selected video properly
+			await sleep(1);
+		}
+		swapToIndex = index;
+	};
+
+	$: console.log(videoList[1]);
 </script>
 
 <PlaylistWrapper permanentScrollTrack={false} --scrollbar-margin-btm="18px">
 	<div id="li-wrapper">
 		{#each videoList as video, index}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 			<li
 				class:active={index == activeVideoIndex}
 				class:hidden={!(
 					containsSubstring(video.title, searchTerm) ||
 					containsSubstring(video.channelTitle, searchTerm)
 				)}
-				on:click={() => {
-					swapToIndex = index;
-					searchTerm = '';
-				}}
+				on:click={handleSwapVideo(index)}
+				bind:this={listElement[index]}
+				class:fade={false}
+				style="--anim-time:{transitionAnimTime}ms; --direction:{directionDown ||
+				index < activeVideoIndex
+					? 1
+					: -1}"
 			>
 				<div class="thumbnail" style:background-image={`url(${video.thumbnailUrl}`} />
 				<div class="video-info">
@@ -83,5 +122,20 @@
 
 	.hidden {
 		display: none;
+	}
+
+	.fade {
+		animation: fade var(--anim-time) linear forwards;
+	}
+
+	@keyframes fade {
+		0% {
+			opacity: 1;
+		}
+
+		100% {
+			opacity: 0;
+			transform: translateY(calc(var(--direction) * 50px));
+		}
 	}
 </style>
--- youtube-randomiser/src/lib/misc/util.ts
+++ youtube-randomiser/src/lib/misc/util.ts
@@ -1,3 +1,5 @@
 export const restringify = (obj: any) => {
 	return JSON.parse(JSON.stringify(obj));
 };
+
+export const sleep = (ms: number): Promise<void> => new Promise((r) => setTimeout(r, ms));
